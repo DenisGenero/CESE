22f50fbe2e4b8018056880b1e7a0b19185a4bc3d
denis genero <denisgenero@live.com.ar>
2023-04-04 08:40:52 -0300

    actualización de archivos

--- ProgMicro/PdM_workspace-main/Practica4_1/Debug/Drivers/API/Src/subdir.mk
+++ ProgMicro/PdM_workspace-main/Practica4_1/Debug/Drivers/API/Src/subdir.mk
@@ -15,13 +15,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Drivers/API/Src/%.o Drivers/API/Src/%.su: ../Drivers/API/Src/%.c Drivers/API/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F429ZITx -DSTM32F4 -DNUCLEO_F429ZI -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Inc -I"/home/denis/Documentos/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/STM32F4xx_HAL_Driver/Inc" -I"/home/denis/Documentos/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/CMSIS/Include" -I"/home/denis/Documentos/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"/home/denis/Documentos/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/BSP/STM32F4xx_Nucleo_144" -I"/home/denis/Documentos/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/Core/Inc" -I"/home/denis/Documentos/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/API" -I"/home/denis/Documentos/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/API/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Drivers/API/Src/%.o Drivers/API/Src/%.su Drivers/API/Src/%.cyclo: ../Drivers/API/Src/%.c Drivers/API/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F429ZITx -DSTM32F4 -DNUCLEO_F429ZI -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Inc -I"D:/UBA/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/UBA/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/CMSIS/Include" -I"D:/UBA/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/UBA/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/BSP/STM32F4xx_Nucleo_144" -I"D:/UBA/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/Core/Inc" -I"D:/UBA/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/API" -I"D:/UBA/CESE/ProgMicro/PdM_workspace-main/Practica4_1/Drivers/API/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Drivers-2f-API-2f-Src
 
 clean-Drivers-2f-API-2f-Src:
-	-$(RM) ./Drivers/API/Src/API_delay.d ./Drivers/API/Src/API_delay.o ./Drivers/API/Src/API_delay.su
+	-$(RM) ./Drivers/API/Src/API_delay.cyclo ./Drivers/API/Src/API_delay.d ./Drivers/API/Src/API_delay.o ./Drivers/API/Src/API_delay.su
 
 .PHONY: clean-Drivers-2f-API-2f-Src
 
