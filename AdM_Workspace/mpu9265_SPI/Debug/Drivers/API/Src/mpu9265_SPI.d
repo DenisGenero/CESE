e5ed1915d6952c7194fcc9e8deb0df3d7f359fb0
denis genero <denisgenero@live.com.ar>
2023-04-29 11:55:20 -0300

    subida de cuestionario en blanco

--- AdM_Workspace/mpu9265_SPI/Debug/Drivers/API/Src/mpu9265_SPI.d
+++
@@ -1,3 +0,0 @@
-Drivers/API/Src/mpu9265_SPI.o: ../Drivers/API/Src/mpu9265_SPI.c \
- D:/UBA/CESE/ProgMicro/PdM_workspace-main/mpu9265_SPI/Drivers/API/Inc/mpu9265_SPI.h
-D:/UBA/CESE/ProgMicro/PdM_workspace-main/mpu9265_SPI/Drivers/API/Inc/mpu9265_SPI.h:
